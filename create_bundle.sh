193e626b87b431adef4e892437314212a0aec325
mdpradeep <pradeep@juliacomputing.com>
2015-10-06 15:23:17 +0000

    removed install script from tar bundle

--- create_bundle.sh
+++ create_bundle.sh
@@ -4,4 +4,5 @@
 ##TODO :  Check if the at bundle exists. If so, either ask to overwrite or move it to a BAK !
 
 ## Tar up all the required images and components
-tar cvf JuliaDeploymentBundle.tar JuliaDeployment JuliaDeploymentImages jboxengine install_julia_deployment.sh
+## tar cvf JuliaDeploymentBundle.tar JuliaDeployment JuliaDeploymentImages jboxengine install_julia_deployment.sh
+tar cvf JuliaDeploymentBundle.tar JuliaDeployment JuliaDeploymentImages jboxengine 
